43bc24f97009fe2672bb984f02bbb54a6b7fe67a
LazarBojanic <bojanic.lazar50@gmail.com>
2023-06-20 03:56:28 +0200

    fetching meals from selected category in spinner

--- app/src/main/java/rs/raf/projekat_jun_lazar_bojanic_11621/activity/MealsSearchActivity.java
+++ app/src/main/java/rs/raf/projekat_jun_lazar_bojanic_11621/activity/MealsSearchActivity.java
@@ -1,28 +1,25 @@
 package rs.raf.projekat_jun_lazar_bojanic_11621.activity;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
 
 import java.util.Collections;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import rs.raf.projekat_jun_lazar_bojanic_11621.R;
-import rs.raf.projekat_jun_lazar_bojanic_11621.adapter.CategoryListAdapter;
 import rs.raf.projekat_jun_lazar_bojanic_11621.adapter.MealListAdapter;
-import rs.raf.projekat_jun_lazar_bojanic_11621.viewmodel.MainActivityViewModel;
 import rs.raf.projekat_jun_lazar_bojanic_11621.viewmodel.MealsSearchActivityViewModel;
 
 public class MealsSearchActivity extends AppCompatActivity {
@@ -30,24 +27,31 @@ public class MealsSearchActivity extends AppCompatActivity {
     private MealListAdapter mealListAdapter;
     private ProgressBar progressBarLoading;
     private String strCategory;
+
+    private String selectedStrCategory;
+    private Spinner spinnerCategoryFilter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meals_search);
-        Intent intent = getIntent();
-        strCategory = intent.getStringExtra(String.valueOf(R.string.extraStrCategory));
-        Log.i(String.valueOf(R.string.foodgeTag), strCategory);
-        mealsSearchActivityViewModel = new ViewModelProvider(this).get(MealsSearchActivityViewModel.class);
+        initializeViews();
+        initializeViewModel();
+        initializeListeners();
+        fetchInitialData();
+    }
 
-
+    private void initializeViews() {
+        spinnerCategoryFilter = findViewById(R.id.spinnerCategoryFilter);
         RecyclerView recyclerViewMealList = findViewById(R.id.recyclerViewMealList);
         recyclerViewMealList.setLayoutManager(new LinearLayoutManager(this));
         progressBarLoading = findViewById(R.id.progressBarLoading);
         mealListAdapter = new MealListAdapter(Collections.emptyList());
         recyclerViewMealList.setAdapter(mealListAdapter);
-        mealListAdapter.setOnMealClickListener(meal -> {
-            //handle on click
-        });
+    }
+
+    private void initializeViewModel() {
+        mealsSearchActivityViewModel = new ViewModelProvider(this).get(MealsSearchActivityViewModel.class);
         mealsSearchActivityViewModel.getMealListLiveData().observe(this, mealList -> {
             mealListAdapter.setMealList(mealList);
         });
@@ -58,18 +62,62 @@ public class MealsSearchActivity extends AppCompatActivity {
                 hideLoadingView();
             }
         });
+        mealsSearchActivityViewModel.getSimpleCategoryListLiveData().observe(this, categories -> {
+            ArrayAdapter<String> spinnerCategoryFilterAdapter = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, categories);
+            spinnerCategoryFilterAdapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+            spinnerCategoryFilter.setAdapter(spinnerCategoryFilterAdapter);
+
+            // Set selected item in spinner
+            if (strCategory != null && !strCategory.isEmpty()) {
+                int index = spinnerCategoryFilterAdapter.getPosition(strCategory);
+                if (index != -1) {
+                    spinnerCategoryFilter.setSelection(index);
+                }
+            }
+        });
+    }
+
+    private void initializeListeners() {
+        mealListAdapter.setOnMealClickListener(meal -> {
+            //handle on click
+        });
+        spinnerCategoryFilter.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedStrCategory = spinnerCategoryFilter.getItemAtPosition(position).toString();
+                Log.i(String.valueOf(R.string.foodgeTag), selectedStrCategory);
+                mealsSearchActivityViewModel.fetchAllMealsForCategory(selectedStrCategory)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+
+    private void fetchInitialData() {
+        Intent intent = getIntent();
+        strCategory = intent.getStringExtra(String.valueOf(R.string.extraStrCategory));
+        Log.i(String.valueOf(R.string.foodgeTag), strCategory);
+
+        mealsSearchActivityViewModel.fetchAllCategories()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe();
 
         mealsSearchActivityViewModel.fetchAllMealsForCategory(strCategory)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe();
-
-
     }
 
     private void showLoadingView() {
         progressBarLoading.setVisibility(View.VISIBLE);
     }
+
     private void hideLoadingView() {
         progressBarLoading.setVisibility(View.GONE);
     }
--- app/src/main/java/rs/raf/projekat_jun_lazar_bojanic_11621/viewmodel/MealsSearchActivityViewModel.java
+++ app/src/main/java/rs/raf/projekat_jun_lazar_bojanic_11621/viewmodel/MealsSearchActivityViewModel.java
@@ -10,27 +10,37 @@ import androidx.lifecycle.ViewModel;
 import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Single;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import rs.raf.projekat_jun_lazar_bojanic_11621.FoodgeApp;
+import rs.raf.projekat_jun_lazar_bojanic_11621.database.remote.model.Category;
 import rs.raf.projekat_jun_lazar_bojanic_11621.database.remote.model.Meal;
+import rs.raf.projekat_jun_lazar_bojanic_11621.database.remote.repository.ICategoryRepository;
 import rs.raf.projekat_jun_lazar_bojanic_11621.database.remote.repository.IMealRepository;
+import rs.raf.projekat_jun_lazar_bojanic_11621.database.remote.response.CategoriesResponse;
 import rs.raf.projekat_jun_lazar_bojanic_11621.util.PlaceHolders;
 
 public class MealsSearchActivityViewModel extends ViewModel {
+    private ICategoryRepository categoryRepository;
     private IMealRepository mealRepository;
+    private MutableLiveData<List<String>> simpleCategoryListLiveData;
     private MutableLiveData<List<Meal>> mealListLiveData;
     private MutableLiveData<Boolean> loadingStatusLiveData;
 
     public MealsSearchActivityViewModel() {
+        simpleCategoryListLiveData = new MutableLiveData<>();
         mealListLiveData = new MutableLiveData<>();
         loadingStatusLiveData = new MutableLiveData<>();
+        categoryRepository = FoodgeApp.getInstance().getRemoteAppComponent().getCategoryRepository();
         mealRepository = FoodgeApp.getInstance().getRemoteAppComponent().getMealRepository();
     }
-
+    public LiveData<List<String>> getSimpleCategoryListLiveData() {
+        return simpleCategoryListLiveData;
+    }
     public LiveData<List<Meal>> getMealListLiveData() {
         return mealListLiveData;
     }
@@ -40,6 +50,7 @@ public class MealsSearchActivityViewModel extends ViewModel {
     }
 
     public Observable<List<Meal>> fetchAllMealsForCategory(String strCategory) {
+        mealListLiveData.setValue(Collections.emptyList());
         return mealRepository.fetchAllMealsByCategory(strCategory)
                 .subscribeOn(Schedulers.io())
                 .doOnSubscribe(disposable -> setLoadingStatus(true))
@@ -83,6 +94,21 @@ public class MealsSearchActivityViewModel extends ViewModel {
                 })
                 .doOnNext(meals -> mealListLiveData.postValue(meals));
     }
+    public Observable<List<String>> fetchAllCategories() {
+        return categoryRepository.fetchAllCategories()
+                .subscribeOn(Schedulers.io())
+                .map(response -> {
+                    List<Category> categories = response.getCategories();
+                    List<String> categoryNames = new ArrayList<>();
+                    for (Category category : categories) {
+                        categoryNames.add(category.getStrCategory());
+                    }
+                    return categoryNames;
+                })
+                .doOnNext(simpleCategoryListLiveData::postValue)
+                .onErrorReturnItem(new ArrayList<>());
+    }
+
 
 
     private void setLoadingStatus(boolean isLoading) {
